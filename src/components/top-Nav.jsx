161976866085a5011e02b60a00b5ba11ea61fd86
1asonn <2554195251@qq.com>
2025-06-09 19:29:25 +0800

    feat:update

---
+++ src/components/top-Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+const TopNav = ({LeftRegion,MiddleRegion,RightRegion}) => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.leftRegion}><LeftRegion/></div>
+            <div className={styles.middleRegion}><MiddleRegion/></div>
+            <div className={styles.rightRegion}><RightRegion/></div>
+        </div>
+    )
+}
+
+
+export default TopNav
---
+++ src/pages/portal/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import TopNav from '../../components/top-Nav'
+import styles from './index.module.scss'
+
+const PortalPage = () => {
+
+    const leftRegion = () =>{
+        return (
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src="/images/music.jpg"/>
+                <span className={styles.logoText}>网易云音乐</span>
+            </div>
+        )
+    }
+
+    const middleRegion = () => {
+        return (
+            <div>
+                <input type="text" placeholder="搜索音乐、歌手、歌词、用户" />
+            </div>
+        )
+    }
+    
+    const rightRegion = () => <span>right</span>
+    return (
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <TopNav
+                    LeftRegion={leftRegion}
+                    MiddleRegion={middleRegion}
+                    RightRegion={rightRegion}
+                />
+            </div>
+            <section>
+                
+            </section>
+        </div>
+    )
+}
+
+
+
+export default PortalPage
